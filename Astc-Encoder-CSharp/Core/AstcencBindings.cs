d38ddc3e2507368d6c713c558f649c936bee27f7
Anthony <Anthonyfrank378@gmail.com>
2026-05-14 14:04:36 -0400

    Refactor Checks for Sve128 and sve256 support

--- Astc-Encoder-CSharp/Core/AstcencBindings.cs
+++ Astc-Encoder-CSharp/Core/AstcencBindings.cs
@@ -196,19 +196,9 @@ namespace AstcEncoder
                     switch (instruction)
                     {
                         case CpuInstruction.SVEC_128:
-#if NET9_0_OR_GREATER
-#pragma warning disable SYSLIB5003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-                            return Sve.IsSupported && Vector128.IsHardwareAccelerated;
-#else
-                            return false;
-#endif
+                            return Vector128.IsHardwareAccelerated;
                         case CpuInstruction.SVEC_256:
-#if NET9_0_OR_GREATER
-                            return Sve.IsSupported && Vector256.IsHardwareAccelerated;
-#pragma warning restore SYSLIB5003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-#else
-                            return false;
-#endif
+                            return Vector256.IsHardwareAccelerated;
                         case CpuInstruction.NEON:
                             return AdvSimd.IsSupported;
                         default:
